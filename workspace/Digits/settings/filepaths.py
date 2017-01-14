ec4c2f11378fa59efa4011094840c9e6a6943950
jchodar <lukasz.chodarcewicz@gmail.com>
2017-01-14 14:56:50 +0100

    added filepaths as settings

---
+++ workspace/Digits/settings/filepaths.py
@@ -0,0 +1,10 @@
+'''
+Created on Jan 14, 2017
+
+@author: lukasz
+'''
+
+filename_data = '/home/lukasz/digits/train.csv'
+filename_test = '/home/lukasz/digits/test.csv'
+filename_output = '/home/lukasz/digits//out/out.csv'
+visualization_prefix = '/home/lukasz/digits/img/'
