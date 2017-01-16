ccc394842d283192733ecf9bdbc78b914087d3f8
zeph <asdf@asdf>
2017-01-17 00:35:36 +0100

    filepaths

--- workspace/Digits/settings/filepaths.py
+++ workspace/Digits/settings/filepaths.py
@@ -4,7 +4,9 @@ Created on Jan 14, 2017
 @author: lukasz
 '''
 
-filename_data = '/home/lukasz/digits/train.csv'
-filename_test = '/home/lukasz/digits/test.csv'
-filename_output = '/home/lukasz/digits//out/out.csv'
-visualization_prefix = '/home/lukasz/digits/img/'
+basepath = 'C:/tensorData'
+
+filename_data = basepath+'/digits/train.csv'
+filename_test = basepath+'/digits/test.csv'
+filename_output = basepath+'/digits/out/out.csv'
+visualization_prefix = basepath+'/digits/img/'
